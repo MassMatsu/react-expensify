449e7adad968e2f0d4b13b7a95d856f76ba8bfa9
MassMatsu <masa10matsukawa@gmail.com>
2021-04-08 09:44:28 +0900

    implemented ExpenseFrom component on CreateExpensePage

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Header from '../components/Header'
 import ExpenseList from '../components/ExpenseList';
 import ExpenseItem from '../components/ExpenseItem'
 import ExpenseListFilters from '../components/ExpenseListFilters'
+import ExpenseForm from '../components/ExpenseForm'
 
-export {Header, ExpenseList, ExpenseItem, ExpenseListFilters}
+export {Header, ExpenseList, ExpenseItem, ExpenseListFilters, ExpenseForm}
