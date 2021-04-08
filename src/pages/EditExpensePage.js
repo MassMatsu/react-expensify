66086108ecdaf5b553db08bc2923c850341ba64e
MassMatsu <masa10matsukawa@gmail.com>
2021-04-08 18:05:26 +0900

    implemented EditExpensePage

--- src/pages/EditExpensePage.js
+++ src/pages/EditExpensePage.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {ExpenseForm} from '../components/index'
+import {editExpense, removeExpense} from '../actions/expenses'
 
 const EditExpensePage = (props) => {
   console.log(props)
-  return<div>
-  editing the expense with id of {props.match.params.id}
-  </div>
+  return <div>
+   <ExpenseForm
+    expense={props.expense}
+    onSubmit={(expense) => {
+      console.log('updated', expense)
+      props.dispatch(editExpense(props.expense.id, expense))
+      props.history.push('/')
+    }}
+   />
+  
+   <button
+    onClick={() => {
+      props.dispatch(removeExpense(props.expense.id))
+      props.history.push('/')
+    }}
+   >remove</button>
+  </div>;
 };
 
-export default EditExpensePage
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+  }
+}
+export default connect(mapStateToProps)(EditExpensePage);
