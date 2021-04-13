b314aaa5d812fd5b1019281a33394a1ecdfe105e
MassMatsu <masa10matsukawa@gmail.com>
2021-04-13 21:52:12 +0900

    implemented test case for CreateExpensePage

--- src/pages/CreateExpensePage.js
+++ src/pages/CreateExpensePage.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import {connect} from 'react-redux'
-import {ExpenseForm} from '../components/index'
-import {addExpense} from '../actions/expenses'
+import { connect } from 'react-redux';
+import { ExpenseForm } from '../components/index';
+import { addExpense } from '../actions/expenses';
 
-const CreateExpensePage = (props) => (
-  <div>
-    <h1>Add Expense</h1>
-    <ExpenseForm onSubmit={(expense) => {
-      props.dispatch(addExpense(expense))
-      props.history.push('/')    // navigate a user to home page when the form submit is done
-    }}/>
-  </div>
-  
-  );
+export class CreateExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.onSubmit(expense);
+    this.props.history.push('/');
+  };
+  render() {
+    return (
+      <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm onSubmit={this.onSubmit} />
+      </div>
+    );
+  }
+}
 
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit: (expense) => dispatch(addExpense(expense)),
+});
 
-export default connect()(CreateExpensePage)
+export default connect(undefined, mapDispatchToProps)(CreateExpensePage);
